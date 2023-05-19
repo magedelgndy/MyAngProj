39660c7692babd4fe3cdb474862f59fbd97e6f84
Maged <maged.elgndy999@gmail.com>
2023-05-19 18:22:20 +0200

    first push my angular website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/MyNavbar/navbar.component';
+import { SliderComponent } from './components/MySlider/slider.component';
+import { MainComponent } from './components/MyMainContent/main.component';
+import { ProudctsComponent } from './components/MyProudcts/proudcts.component';
+import { FooterComponent } from './components/MyFooter/footer.component';
+import { AboutComponent } from './components/MyAboutSection/about.component';
+import { ProudctComponent } from './components/MyProuduct/proudct.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent,
+    MainComponent,
+    ProudctsComponent,
+    FooterComponent,
+    AboutComponent,
+    ProudctComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/MyProudcts/proudcts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proudcts',
+  templateUrl: './proudcts.component.html',
+  styleUrls: ['./proudcts.component.css']
+})
+export class ProudctsComponent {
+
+}
